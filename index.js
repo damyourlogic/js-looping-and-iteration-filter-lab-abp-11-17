261e4b99d4159a8a0cfca9556496a067480d12b9
Amy Jackson <aj81623@gmail.com>
2017-11-14 00:40:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching (list, name){
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  })
+}
+function fuszzyMatch(list, partialName){
+  
+}
